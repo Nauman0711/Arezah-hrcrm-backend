b514fe59cf6fec80c99feb4ba427ec65c30ebc49
Nauman Babar <47433366+Nauman0711@users.noreply.github.com>
2025-09-17 05:31:41 +0500

    node mailer missing packages done

--- src/routes/employes/add/index.js
+++ src/routes/employes/add/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../../../models/user');
 const authMiddleware = require('../../../middlewares/auth');
+const authorize = require('../../../middlewares/authorize');
 
 router.post('/', authMiddleware, authorize(['ceo', 'admin', 'manager']), async (req, res) => {
     const {
